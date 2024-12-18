4321491397d30b4b2e125bfcc5540f0f20acf7b5
shalu-patel <shalupatel1497@gmail.com>
2024-12-18 12:36:33 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Krish", lastName: "Patel" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling on the route");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling on the route user 2");
+    // res.send("Response 2!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling on the route user 3");
+    // res.send("Response 3!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling on the route user 4");
+    res.send("Response 4!");
+  }
+);
 
 app.listen(1818, () => {
   console.log("Server is successfully listing to port 1818....");
